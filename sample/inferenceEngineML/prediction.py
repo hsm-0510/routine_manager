37c59cc7053f731922dd6952cfde1890e9a4ceda
hasemhasan <sm.hasemhasan@gmail.com>
2026-04-14 15:01:36 +0500

    minor_changes
    -> code rearrangement
    -> function relocations
    -> test7 simplification

--- sample/inferenceEngineML/prediction.py
+++ sample/inferenceEngineML/prediction.py
@@ -1,4 +1,4 @@
-import os
+import os, time, traceback
 from sample.inferenceEngineML.buffer_manager import BufferManager
 from sample.inferenceEngineML.feature_builder import build_features
 from sample.inferenceEngineML.model_runner import ModelRunner
@@ -24,23 +24,18 @@ class PredictionEngine:
 
     def process_event(self, event):
         
-        
         signature = (
             event.get("COMPARTMENT_NUMBER"),
             event.get("GROSS_QUANTITY"),
             event.get("Net_Weight_Transformed")
         )
-        
-        # signature = tuple(sorted(event.items()))
 
         if signature == self.last_signature:
             return None
 
         self.last_signature = signature
-
         trailer = self.buffer.add_event(event)
-
-        rows = self.buffer.get_buffer(trailer)
+        rows = self.buffer.get_buffer(trailer)      
         
         if len(rows) < 1:
             return None
@@ -52,7 +47,6 @@ class PredictionEngine:
         label, probs = self.model.predict(X)
 
         self.buffer.mark_processed(trailer)
-        
         if self.buffer.size(trailer) < self.MIN_COMPARTMENTS:
             return None
         else:
@@ -66,3 +60,100 @@ class PredictionEngine:
             "PROB_THEFT": probs["THEFT"],
             "PROB_MISSING": probs["MISSING"]
         }
+
+# Data Mapping for ML-variables & OPC-tags
+def normalize_event(event):
+    return {
+        "TRAILER_CODE": event.get("SAP_trailer_code"),
+        "ORDERED_QUANTITY": event.get("SAP_ordered_quantity"),
+        "GROSS_QUANTITY": event.get("SAP_gross_quantity"),
+        "Density": event.get("SAP_material_density"),
+        "START_TIME": event.get("SAP_batch_start_time"),
+        "END_TIME": event.get("SAP_batch_end_time"),
+        "COMPARTMENT_NUMBER": event.get("SAP_compartment_number"),
+        "Net_Weight_Transformed": event.get("SAP_trailer_net_weight"),
+        "Net_Weight_Expected": event.get("SAP_expected_net_weight"),
+        "NAME": event.get("SAP_compartment_name")
+    }
+
+# ML Inference Worker Function (Thread)
+def ml_inference_worker(opc, engine):
+
+    print("[ML] Inference worker started...")
+    last_event = None
+
+    while True:
+        try:
+            last_version = -1
+            
+            print("\n[ML] --- New Cycle ---")
+
+            # Read State Safely
+            raw_event = dict(state_manager.misc_tags["SAP_DATA"])
+            print("[OPC RAW]", raw_event)
+            
+            # Data unavailability checks
+            if not raw_event:
+                time.sleep(0.5)
+                continue
+            event = normalize_event(raw_event)
+            print("[ML] Raw event read:", event)
+            if not event:
+                print("[ML] No SAP_DATA available, waiting...")
+                time.sleep(0.5)
+                continue
+            if "TRAILER_CODE" not in event:
+                print("[ML] Missing TRAILER_CODE, skipping...")
+                time.sleep(0.5)
+                continue
+
+            # Get trailer code
+            trailer = event.get("TRAILER_CODE")
+            print(f"[ML] Trailer detected: {trailer}")
+
+            # Prevent reprocessing of same data
+            last_event = event.copy()
+            print("[ML] New event accepted for processing")
+
+            # ML pre-run checks
+            current_version = state_manager.get_sap_version()
+            if current_version == last_version:
+                time.sleep(0.2)
+                continue
+            last_version = current_version
+            
+            # Run ML prediction
+            print("[ML] Sending event to PredictionEngine...")
+            result = engine.process_event(event)
+            
+            # In-case of no prediction results
+            if not result:
+                print("[ML] No prediction triggered (buffer not ready or conditions not met)")
+                time.sleep(0.5)
+                continue
+            print("[ML RESULT]", result)
+
+            # Report ML prediction results to designated OPC-tags
+            try:
+                print("[ML] Writing results back to OPC...")
+                opc.write_tag("SAP_DATA", "SAP_PREDICTED_CASE", result["PREDICTED_CASE"])
+                print(f"[ML] Wrote PREDICTED_CASE: {result['PREDICTED_CASE']}")
+                opc.write_tag("SAP_DATA", "SAP_PROB_NORMAL", float(result["PROB_NORMAL"]))
+                print(f"[ML] Wrote PROB_NORMAL: {result['PROB_NORMAL']}")
+                opc.write_tag("SAP_DATA", "SAP_PROB_DRIFT", float(result["PROB_DRIFT"]))
+                print(f"[ML] Wrote PROB_DRIFT: {result['PROB_DRIFT']}")
+                opc.write_tag("SAP_DATA", "SAP_PROB_THEFT", float(result["PROB_THEFT"]))
+                print(f"[ML] Wrote PROB_THEFT: {result['PROB_THEFT']}")
+                opc.write_tag("SAP_DATA", "SAP_PROB_MISSING", float(result["PROB_MISSING"]))
+                print(f"[ML] Wrote PROB_MISSING: {result['PROB_MISSING']}")
+                print("[ML] OPC write completed successfully")
+
+            except Exception as e:
+                print("[ML ERROR] OPC write failed:", e)
+
+            time.sleep(0.5)
+
+        except Exception as e:
+            print("[ML ERROR] Exception:", repr(e))
+            traceback.print_exc()
+            time.sleep(1)
--- tests/test7_sap_opc.py
+++ tests/test7_sap_opc.py
@@ -11,140 +11,11 @@ from sample.tcpClient import tcp_connection_manager
 from sample.utils import config_loader
 from sample.core import state_manager
 from sample.core import controller
+from sample.inferenceEngineML import prediction
 from sample.inferenceEngineML.prediction import PredictionEngine
 
-# ============================
 # GLOBAL ML ENGINE
-# ============================
 engine = PredictionEngine()
-
-def normalize_event(event):
-    def safe_float(x, default=0.0):
-        try:
-            if x is None:
-                return default
-            return float(x)
-        except:
-            return default
-
-    return {
-        "TRAILER_CODE": event.get("SAP_trailer_code"),
-        "ORDERED_QUANTITY": event.get("SAP_ordered_quantity"),
-        "GROSS_QUANTITY": event.get("SAP_gross_quantity"),
-        "Density": event.get("SAP_material_density"),
-        "START_TIME": event.get("SAP_batch_start_time"),
-        "END_TIME": event.get("SAP_batch_end_time"),
-        "COMPARTMENT_NUMBER": event.get("SAP_compartment_number"),
-        "Net_Weight_Transformed": event.get("SAP_trailer_net_weight"),
-        "Net_Weight_Expected": event.get("SAP_expected_net_weight"),
-        "NAME": event.get("SAP_compartment_name")
-    }
-
-# ============================
-# ML INFERENCE WORKER THREAD
-# ============================
-def ml_inference_worker(opc):
-
-    print("[ML] Inference worker started...")
-
-    last_event = None
-
-    while True:
-        try:
-            last_version = -1
-            
-            print("\n[ML] --- New Cycle ---")
-
-            # -------------------------------
-            # READ STATE SAFELY
-            # -------------------------------
-            raw_event = dict(state_manager.misc_tags["SAP_DATA"])
-            print("[OPC RAW]", raw_event)
-            
-            if not raw_event:
-                time.sleep(0.5)
-                continue
-            event = normalize_event(raw_event)
-            print("[ML] Raw event read:", event)
-            
-            if not event:
-                print("[ML] No SAP_DATA available, waiting...")
-                time.sleep(0.5)
-                continue
-
-            if "TRAILER_CODE" not in event:
-                print("[ML] Missing TRAILER_CODE, skipping...")
-                time.sleep(0.5)
-                continue
-
-            trailer = event.get("TRAILER_CODE")
-            print(f"[ML] Trailer detected: {trailer}")
-
-            # # -------------------------------
-            # # PREVENT REPROCESSING SAME DATA
-            # # -------------------------------
-            # if event == last_event:
-            #     print("[ML] Duplicate event detected, skipping...")
-            #     time.sleep(0.5)
-            #     continue
-
-            last_event = event.copy()
-            print("[ML] New event accepted for processing")
-
-            # -------------------------------
-            # RUN ML
-            # -------------------------------
-            
-            current_version = state_manager.get_sap_version()
-
-            if current_version == last_version:
-                time.sleep(0.2)
-                continue
-
-            last_version = current_version
-            
-            print("[ML] Sending event to PredictionEngine...")
-            result = engine.process_event(event)
-
-            if not result:
-                print("[ML] No prediction triggered (buffer not ready or conditions not met)")
-                time.sleep(0.5)
-                continue
-
-            print("[ML RESULT]", result)
-
-            # -------------------------------
-            # WRITE BACK TO OPC
-            # -------------------------------
-            try:
-                print("[ML] Writing results back to OPC...")
-
-                opc.write_tag("SAP_DATA", "SAP_PREDICTED_CASE", result["PREDICTED_CASE"])
-                print(f"[ML] Wrote PREDICTED_CASE: {result['PREDICTED_CASE']}")
-
-                opc.write_tag("SAP_DATA", "SAP_PROB_NORMAL", float(result["PROB_NORMAL"]))
-                print(f"[ML] Wrote PROB_NORMAL: {result['PROB_NORMAL']}")
-
-                opc.write_tag("SAP_DATA", "SAP_PROB_DRIFT", float(result["PROB_DRIFT"]))
-                print(f"[ML] Wrote PROB_DRIFT: {result['PROB_DRIFT']}")
-
-                opc.write_tag("SAP_DATA", "SAP_PROB_THEFT", float(result["PROB_THEFT"]))
-                print(f"[ML] Wrote PROB_THEFT: {result['PROB_THEFT']}")
-
-                opc.write_tag("SAP_DATA", "SAP_PROB_MISSING", float(result["PROB_MISSING"]))
-                print(f"[ML] Wrote PROB_MISSING: {result['PROB_MISSING']}")
-
-                print("[ML] OPC write completed successfully")
-
-            except Exception as e:
-                print("[ML ERROR] OPC write failed:", e)
-
-            time.sleep(0.5)
-
-        except Exception as e:
-            print("[ML ERROR] Exception:", repr(e))
-            traceback.print_exc()
-            time.sleep(1)
 
 def main():
     # Keep main thread alive
@@ -163,7 +34,7 @@ def main():
         threading.Thread(target=tcp_client.receive_data, args=(conn_mgr,), daemon=True).start()
         
         # ML Inference Thread
-        threading.Thread(target=ml_inference_worker, args=(opc,), daemon=True).start()
+        threading.Thread(target=prediction.ml_inference_worker, args=(opc,engine,), daemon=True).start()
         
         while True: # TCP/OPCUA Data-transfer
             opcua_update.update_opc_elements(opc)
@@ -188,9 +59,6 @@ if __name__ == "__main__":
     
     # Establishing TCP Connection Manager
     conn_mgr = tcp_connection_manager.TCPConnectionManager(tcp_client.SERVER_IP, tcp_client.SERVER_PORT)
-    
-    # OPCUA Update Thread
-    # threading.Thread(target=opcua_update.update_opc_elements, args=(opc, sock,), daemon=True).start()
     
     #Run Main()
     main()
